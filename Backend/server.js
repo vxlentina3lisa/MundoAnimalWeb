a99b6d887daf6febd25584a24465f9e5cd01aa8a
Valentina Elisa <valentinaelisa2022@gmail.com>
2025-06-19 06:09:58 -0400

    arreglines6

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const path = require('path');
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+require('dotenv').config();
 
 const usuarioRoutes = require('./src/server/routes/usuarioRoutes');
 const productoRoutes = require('./src/server/routes/productoRoutes');
@@ -15,18 +11,27 @@ const protectedRoutes = require('./src/server/routes/protectedRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://mundoanimalweb-frontend.onrender.com'
+];
 app.use(cors({
-  origin: 'https://mundoanimalweb-frontend.onrender.com'
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS no permitido'));
+    }
+  },
+  credentials: true
 }));
+
 app.use(express.json());
-
 app.use('/assets/imagenes', express.static(path.join(__dirname, 'public/assets/imagenes')));
-
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/productos', productoRoutes);
 app.use('/api/carrito', carritoRoutes);
 app.use('/api/protected', protectedRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
